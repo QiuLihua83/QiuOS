bdd2357431a6c54a9e8664131cf1d83147074bf2
qiulihua83 <qiulihua83@163.com>
2015-06-29 23:48:31 +0800

    complete

--- gdt/gdt.asm
+++ gdt/gdt.asm
@@ -3,7 +3,7 @@
 
 
 org 07c00h
-;jmp LABEL_BEGIN
+jmp LABEL_BEGIN
 
 [SECTION .gdt]
 
@@ -23,16 +23,51 @@ SelectorVideo  equ  LABEL_DESC_VIDEO-LABEL_GDT
 
 [SECTION .s16]
 [BITS 16]
-LABLE_BEGIN:
+LABEL_BEGIN:
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0100h
 
+    xor eax, eax
+    mov ax, cs
+    shl eax, 4
+    add eax, LABEL_SEG_CODE
+    mov word [LABEL_DESC_CODE+2], ax
+    shr eax, 16
+    mov byte [LABEL_DESC_CODE+4], al
+    mov byte [LABEL_DESC_CODE+7], ah
+
+    xor eax, eax
+    mov ax, ds
+    shl eax, 4
+    add eax, LABEL_GDT
+    mov dword [Gdtptr+2], eax
+
+    lgdt  [Gdtptr]
+
+    cli
+
+    in al, 92h
+    or al, 00000010b
+    out 92h, al
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
 
     jmp SelectorCode:0
 
 [SECTION .s32]
 [BITS 32]
 LABEL_SEG_CODE:
-
-
+    mov ax, SelectorVideo
+    mov gs,ax
+    mov edi, (80*6+20)*2
+    mov ah, 0Ch
+    mov al,'Q'
+    mov [gs:edi], ax
 
     jmp $
 
