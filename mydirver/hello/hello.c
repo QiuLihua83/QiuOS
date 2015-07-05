f82f620b1d6cd46706d5f4d5af41a75dbce27875
qiulihua <qiulihua83@gmail.com>
2015-07-05 15:05:54 +0800

    dev init and release

--- mydirver/hello/hello.c
+++ mydirver/hello/hello.c
@@ -8,15 +8,49 @@
 #include "linux/fs.h"
 #include "linux/fcntl.h"
 
+const unsigned int DEV_NUM = 1;
+
+static dev_t  dev;
+
+
+
+void initDev(void);
+void releaseDev(void);
+
 static int __init Init(void)
 {
     printk(KERN_ALERT "hello qiulihua, initializing now......\n");
     printk(KERN_ALERT "current process is : name=%s, pid=[%i]\n", current->comm, current->pid);
+   
+   initDev(); 
     return 0;
 }
 
+void initDev(void)
+{
+    int ret = 0;
+    ret = alloc_chrdev_region(&dev, 0, DEV_NUM, "hellodev");
+    if(0 == ret)
+    {
+         printk(KERN_ALERT "init dev succed! major=%i, minor=%i\n", MAJOR(dev),MINOR(dev));   
+    }
+   else
+    {
+        printk(KERN_ALERT "init failed! ret = %i\n", ret);
+    }
+   
+    return;
+}
+
+void releaseDev(void)
+{
+    unregister_chrdev_region(dev, DEV_NUM);
+}
+
+
 static void __exit Exit(void)
 {
+    releaseDev();
     printk(KERN_ALERT "hello qiulihua, exit now, bye bye !\n");
 }
 
